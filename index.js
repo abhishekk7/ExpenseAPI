b5d5dabb2faddbbbcb29c0629d1f6a403ba1e0af
abhishekk7 <abhishek3433kumar@gmail.com>
2017-10-16 20:29:32 -0500

    changed api routes
    added morgan

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const dbConfig = require('./config/db');
-const routes = require('./routes/api');
+const routes = require('./apiRoutes/routes');
 
 // setup express app
 const app = express();
@@ -15,9 +16,28 @@ mongoose.connect(dbConfig.address);
 mongoose.Promise = global.Promise;
 
 // middleware
-app.use(bodyParser.json());
-app.use('/api', routes);
 
+app.use(morgan('dev'));
+// parse application/json 
+app.use(bodyParser.json());
+
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+}));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use('/api', routes);
 
 // error handling middleware
 app.use(function (err, req, res, next) {
